3f6c720972687ef184fa8b4d29b913a4dd8aef1e
unknown <ch.paronda@gmail.com>
2019-09-28 10:12:11 +0800

    removed bootstrap

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import BootstrapVue from 'bootstrap-vue'
+
 import VueRouter from 'vue-router'
 import {routes} from './routes';
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+
 Vue.use(VueRouter)
 
 
